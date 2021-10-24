96684843275f6c697c6c9a5381b114e3f33b5ac6
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-10-24 12:33:43 -0700

    extra credit 3

---
+++ src/exercise/06.extra-3.js
@@ -0,0 +1,39 @@
+// Controlled Forms
+// http://localhost:3000/isolated/exercise/06.extra-3.js
+
+import * as React from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [username, setUsername] = React.useState('')
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    onSubmitUsername(username)
+  }
+
+  function handleChange(event) {
+    setUsername(event.target.value.toLowerCase())
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          onChange={handleChange}
+          value={username}
+          id="username"
+          type="text"
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
